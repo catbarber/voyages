8a24f82bdd688267c2b439767204a8a4bacd4fe4
fevjitsu <christopher.feveck@gmail.com>
2025-01-18 17:28:21 -0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from "react";
 import voyagesOne from "/voyages1.png";
 import "./App.css";
-import DownloadPDFVol1 from "./components/DownloadPDFVol1.jsx";
-import DownloadEPUBVol1 from "./components/DownloadEPUBVol1.jsx";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import FacebookIcon from "@mui/icons-material/Facebook";
-
+import XIcon from "@mui/icons-material/X";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div
@@ -22,6 +17,8 @@ function App() {
             href="https://www.facebook.com/profile.php?id=61569913277354"
             target="_blank">
             <FacebookIcon />
+            <br />
+            <span>fACEBOOK</span>&nbsp;&nbsp;
           </a>
         </div>
         <div>
@@ -29,14 +26,23 @@ function App() {
             href="https://www.instagram.com/the_voyages_of_victora/profilecard/?igsh=eHUxd2J5MDNsMmh4"
             target="_blank">
             <InstagramIcon />
+            <br />
+            <span>Instagram</span>&nbsp;&nbsp;
+          </a>
+        </div>
+        <div>
+          <a href="https://x.com/victora_voyages" target="_blank">
+            <XIcon />
+            <br />
+            <span>X.com</span>
           </a>
         </div>
       </div>
       <br />
       <h3>
         Author:&nbsp;
-        <a href="https://x.com/chris_feveck" target="_blank">
-          Chris Feveck
+        <a href="https://chris-feveck.com" target="_blank">
+          Christopher Feveck
         </a>
       </h3>
       <h1>The Voyages of Victora.</h1>
@@ -53,18 +59,23 @@ function App() {
         <div className="series-list-item">
           <h3>The Voyages of Victora : Volume one</h3>
           <div>
-            <img src={voyagesOne} width={440} />
+            <img
+              src={voyagesOne}
+              width={440}
+              alt="A pirate ship at night on rough seas."
+            />
           </div>
           <div>
-            <DownloadPDFVol1 />
-            &nbsp;
-            <DownloadEPUBVol1 />
-            &nbsp;
-            <button style={{ margin: "1rem" }}>
+            <button
+              style={{
+                margin: "1rem",
+                backgroundColor: "#292b2c",
+              }}>
               <a
-                href="https://docs.google.com/document/d/e/2PACX-1vR1dUdgk4A1B1bVBE-LqsCe993vsW9kc4EGpjM1aR1x-yZPJdfhxncWV_Ndu-yWBsJ4nVtNy7p6iXXA/pub"
+                style={{ color: "#f7f7f7" }}
+                href="https://www.amazon.ca/dp/B0DQJWVS97/ref=cbw_us_ca_dp_narx_gl_book"
                 target="_blank">
-                Link To EBook
+                Paperback Edition
               </a>
             </button>
           </div>
@@ -81,6 +92,17 @@ function App() {
               that captures the spirit of exploration and the thrill of the
               hunt. Join Captain Bartley and his crew as they navigate the
               perils of the sea in pursuit of glory and fortune.
+            </span>
+            <span className="invisible">
+              pirate fantasy, young adult fantasy, fantasy adventure, pirate
+              books for teens, seafaring fantasy, pirate fantasy with magic,
+              fantasy pirates on the high seas, young adult fantasy pirates,
+              swashbuckling fantasy, mythical creatures at sea, female pirate,
+              young adult fantasy with strong female protagonist, mythical
+              creatures at sea, fantasy pirates with powers, dark pirate
+              fantasy, humorous pirate fantasy, gritty pirate fantasy, best
+              young adult pirate fantasy novels, fantasy books about pirates for
+              teenagers, where t find a good pirate fantasy books
             </span>
           </p>
         </div>
